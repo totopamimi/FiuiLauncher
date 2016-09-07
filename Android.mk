599acfb7a077ad9df24ba9842269ae8a03fa0d28
totopamimi <totopamimi@gmail.com>
2016-09-07 14:30:08 +0800

    FiuiLauncher: update CERTIFICATE

--- Android.mk
+++ Android.mk
@@ -40,11 +40,11 @@ LOCAL_SDK_VERSION := 21
 
 LOCAL_PACKAGE_NAME := FiuiLauncher
 LOCAL_PRIVILEGED_MODULE := true
-#LOCAL_CERTIFICATE := shared
+LOCAL_CERTIFICATE := shared
 
 LOCAL_AAPT_FLAGS += --rename-manifest-package org.fiui.launcher
 
-LOCAL_OVERRIDES_PACKAGES := Launcher2
+LOCAL_OVERRIDES_PACKAGES := HOME Launcher2 Launcher3 Trebuchet
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
